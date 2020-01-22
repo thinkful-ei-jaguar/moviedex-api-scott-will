ef3b7c0c017e5d8b97b5bf59f1abff57a1fc4cc8
Scott Lingner <slingner@gmail.com>
2020-01-22 12:05:28 -0800

    added some boilerplate and beginning stuff from lecture

--- app.js
+++ app.js
@@ -13,9 +13,26 @@ const movies= require('./movieData.js');
 
 const API_TOKEN = process.env.API_TOKEN
 
-app.get('/movie', (req, res) => {
+function handleMovies(req, res) {
   res.json(movies);
-});
+}
+
+function validateBearerToken(req, res, next) {
+  const authVal = req.get("Authorization") || " ";
+
+  if(!authVal.startsWith('Bearer')) {
+    return res.status(400).json({ error: "Missing or incorrect Authorization header"});
+  }
+  const token = authVal.split(' ')[1]
+  if(token !== API_TOKEN) {
+    return res.status(401).json("Invalid credentials");
+  }
+
+  next();
+}
+
+app.get('/movie', validateBearerToken. handleMovies);
+
 
 app.listen(8000, () => {
   console.log('Server started on PORT 8000');
